53723ff0b02e4c0d488d9909cb1e67048ef7b535
Guillaume Gay <guillaume@damcb.com>
2020-05-30 10:51:24 +0200

    changed site URL

--- publishconf.py
+++ publishconf.py
@@ -12,7 +12,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://tyssue.github.io"
+SITEURL = "https://tyssue.io"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
